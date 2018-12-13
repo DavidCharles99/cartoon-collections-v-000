c56db15ff0d654b292afa9f2dafbac864633bf5e
David Charles <ratlover03@gmail.com>
2018-12-13 22:22:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,17 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  	dwarves.each_with_index do |name, i|
+    number = i + 1
+    puts "#{number}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+    planeteer_calls.map {|element| element.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls(array)
+  
 end
 
 def find_the_cheese# code an argument here
